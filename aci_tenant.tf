75c7e61bbf7e0b4d577d77c2c4139ba59afa9624
mikewiegering <mike.wiegering@gmail.com>
2020-11-11 18:07:10 +0000

    changed aci tenant

--- aci_tenant.tf
+++ aci_tenant.tf
@@ -1,16 +1,16 @@
 resource "aci_tenant" "tftenant" {
   name   = "tftenant"
-  annotation  = (known after apply)
+  
 }
 
 resource "aci_vrf" "tfvrf" {
   tenant_dn    = "uni/tn-tftenant"
   name         = "test_vrf_1"
-  annotation = (known after apply)
+  
 }
 
 
 resource "aci_application_profile" "tfapp" {
   tenant_dn     = "uni/tn-tftenant"
   name          = "access"
-  annotation    = (known after apply)
+}
